252614cb592c8757e28fe89ab564e1bf82e2897c
jaketechs <raviakvarian@gmail.com>
2026-09-01 22:46:06 +0700

    add endpoint metrics and range

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import Fastify from "fastify"
 import { cekDb } from "./db/client"
 import { startAgent } from "./agent"
-import { routes } from "./routes/status"
+import { routes as routesStatus } from "./routes/status"
+import{routes as routesMetrics}from "./routes/metrics"
+import {routes as routesRange}from "./routes/range"
 async function mainFunction(){
     await cekDb()
     const app=Fastify()
-    await app.register(routes)
+    await app.register(routesStatus)
+    await app.register(routesMetrics)
+    await app.register(routesRange)
     await app.listen({port:3000,host:"0.0.0.0"})
     console.log("api running on port 3000")
     startAgent()
+ 
 }mainFunction()
--- src/routes/range.ts
+++ src/routes/range.ts
@@ -5,6 +5,9 @@ import Fastify, { FastifyInstance, FastifyPluginOptions } from "fastify";
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
     fastify.get<{Querystring:{from:string; to: string}}>("/api/metrics/range",async(request,reply)=>{
     const {from, to}=request.query
+    if(from ===undefined || to===undefined){
+    return reply.status(400).send({message:"from and to request are missing "})
+}
     const latest =await db
     .select()
     .from(metricsTable)
