e7a82785b4f15bd1f71a897e91b1b302265306f3
jaketechs <raviakvarian@gmail.com>
2026-09-02 23:42:50 +0700

    fix routes and add deploys.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,3 +6,10 @@ export const metricsTable=pgTable("metrics",{
     mem_pct:real(),
     http_ok:boolean().default(true),
 });
+export const deploysTable=pgTable("deploys",{
+    id:integer().primaryKey().generatedAlwaysAsIdentity(),
+    ts:timestamp('timestamp1').notNull().defaultNow(),
+    commit_sha:varchar({length:40}),
+    status:varchar({length:50}),
+    branch:varchar({length:40}),
+})
