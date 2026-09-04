577997923a132820d54481ea875e2c796e2ff953
jaketechs <raviakvarian@gmail.com>
2026-09-04 23:29:54 +0700

    add health

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,18 @@ import { startAgent } from "./agent"
 import { routes as routesStatus } from "./routes/status"
 import{routes as routesMetrics}from "./routes/metrics"
 import {routes as routesRange}from "./routes/range"
-import {routes as routesWebhook}from "./routes/deploys"
-import {routes as routesHealth} from "./routes/healthdeploy"
+import {routes as routesDeploys}from "./routes/deploys"
+import {routes as routesWebhook} from "./routes/routesWebhook"
+import{routes as routesHealth}from "./routes/routesHealth"
 async function mainFunction(){
     await cekDb()
     const app=Fastify()
     await app.register(routesStatus)
     await app.register(routesMetrics)
     await app.register(routesRange)
-    await app.register(routesWebhook)
+    await app.register(routesDeploys)
     await app.register(routesHealth)
+    await app.register(routesWebhook)
     await app.listen({port:3000,host:"0.0.0.0"})
     console.log("api running on port 3000")
     startAgent()
--- src/routes/routesHealth.ts
+++ src/routes/routesHealth.ts
@@ -1,8 +1,27 @@
-import { desc } from "drizzle-orm";
+import { desc,eq,lt,gt} from "drizzle-orm";
 import { db } from "../db/client";
 import { metricsTable,deploysTable } from "../db/schema";
 import Fastify, { FastifyInstance,FastifyPluginOptions}from "fastify"
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
 fastify.get<{Params:{id:string}}>("/api/deploys/:id/health", async (request, reply) => {
   const { id } = request.params;
+  const targetDeploy=await db
+  .select().from(deploysTable).where(eq(deploysTable.id,parseInt(id)))
+  const deployTs = targetDeploy[0].ts;
+  const beforeMetric=await db.select().from(metricsTable).where(lt(metricsTable.ts,deployTs))
+  const afterMetric=await db.select().from(metricsTable).where(gt(metricsTable.ts,deployTs))
+  const totalCpuBefore=beforeMetric.reduce((sum,row)=>sum+(row.cpu_pct?? 0),0);
+  const averageCpuBefore=totalCpuBefore/beforeMetric.length;
+  const totalCpuAfter=afterMetric.reduce((sum,row)=>sum+(row.cpu_pct??0),0);
+  const averageCpuAfter=totalCpuAfter/afterMetric.length;
+  const totalMemoryBefore=beforeMetric.reduce((sum,row)=>sum+(row.mem_pct??0),0);
+  const averageMemoryBefore=totalMemoryBefore/beforeMetric.length;
+  const totalMemAfter=afterMetric.reduce((sum,row)=>sum+(row.mem_pct??0),0)
+  const averageMemAfter=totalMemAfter/afterMetric.length
+  let verdict="healthy"
+  if(averageCpuAfter>averageCpuBefore){verdict="degraded"}
+  if(averageMemAfter>averageMemoryBefore){verdict="degraded"}
+  const goesDown=afterMetric.some(row=>!row.http_ok)
+  if(goesDown){verdict="degraded"}
+  return{verdict,averageCpuBefore,averageCpuAfter,averageMemoryBefore,averageMemAfter}
 })}
